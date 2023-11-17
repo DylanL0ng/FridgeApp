579b34cf095e0ca777f32cd3f960492d62f44b81
Opod <48951218+DylanL0ng@users.noreply.github.com>
2023-11-17 06:13:07 +0000

    Add Item List

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("com.google.zxing:core:3.4.1")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("com.android.volley:volley:1.2.1")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
 
 
     testImplementation("junit:junit:4.13.2")
